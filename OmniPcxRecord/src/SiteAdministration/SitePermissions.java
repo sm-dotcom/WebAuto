9a3bc87aae8980837de95f5d9b5aaa25c443cfa7
Administrator <Administrator@172.20.12.26>
2021-10-04 18:27:18 +0500

    [Sarah] -- Testcase added.

--- OmniPcxRecord/src/SiteAdministration/SitePermissions.java
+++ OmniPcxRecord/src/SiteAdministration/SitePermissions.java
@@ -347,10 +347,10 @@ public class SitePermissions {
 		
 	  ((SharedFunctions)sf).clickSitePermissions(driver);
 				
-      // Make admin copy
-      driver.findElement(By.xpath("//*[@id=\"gvGroups\"]/tbody/tr[1]/td[3]/div/img[2]")).click();
+//      // Make admin copy
+//      driver.findElement(By.xpath("//*[@id=\"gvGroups\"]/tbody/tr[1]/td[3]/div/img[2]")).click();
       
-      //Get the table and row
+        //Get the table and row
 	  	WebElement table1 =driver.findElement(By.id("gvGroups"));
 	  	WebElement tbody1=table1.findElement(By.tagName("tbody"));
 	  	List<WebElement> rows1=tbody1.findElements(By.tagName("tr"));
@@ -377,15 +377,14 @@ public class SitePermissions {
 	  	driver.findElement(By.xpath("//*[@id=\"gvPermissions\"]/tbody/tr[4]/td[1]/img")).click();
 	  	
 	    // Check Permissions (search calls)
-	  	driver.findElement(By.id("1-6")).click();; //	
+	  	driver.findElement(By.id("1-6")).click(); //	
 	  
 	    // Click on teams
 	  	driver.findElement(By.xpath("//*[@id=\"gvPermissions\"]/tbody/tr[9]/td[1]/img")).click();
 	  	
 	    // Check Permissions (teams)
-	  	driver.findElement(By.id("5-28")).click();; //
-	  	
-	  	
+	  	driver.findElement(By.id("5-28")).click(); //
+	  		  	
 	  	Assert.assertTrue(true);
 	}		
 
